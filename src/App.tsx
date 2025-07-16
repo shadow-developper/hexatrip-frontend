b952db085769756142b7fb7ae6905edbfa1f1e60
shadow-developper <axel.barth@yahoo.com>
2025-07-16 16:43:46 +0200

    Routage - Épisode n°90

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-const App = () => {
-  return (
-    <div className="font-bold">Lorem ipsum dolor sit amet.</div>
-  );
-};
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "research", element: <ResearchPage /> },
+      { path: "research/:id", element: <SingleTripPage /> },
+      { path: "advisors", element: <AdvisorsPage /> },
+      { path: "advisors/:id", element: <AdvisorsSinglePage /> },
+      { path: "checkout/", element: <CheckoutPage /> },
+      { path: "agencies", element: <AgenciesPage /> },
+      { path: "hotline", element: <HotlinePage /> },
+      { path: "profile", element: <ProfilePage /> },
+    ],
+  },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/register", element: <RegisterPage /> },
+  { path: "/checkout-success", element: <CheckoutSuccessPage /> },
+]);
