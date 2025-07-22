d82438cbe9bfd9d462d8f3128d9810572b17fa22
shadow-developper <axel.barth@yahoo.com>
2025-07-22 12:01:38 +0200

    UserBar - Épisode n°95

--- src/components/UserBar.tsx
+++ src/components/UserBar.tsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 
 const UserBar = () => {
   return (
-	<div>
-    <Button>
-        click me
-    </Button>
-  </div>
-  )
-}
+    <header className="border-b-2 bg-slate-100">
+      <div className="align-center flex justify-end py-1">
+        <div className="flex items-center gap-6">
+          <Button asChild variant="link" size="lg" className="hover-btn p-0">
+            <Link to="/login">Sign In</Link>
+          </Button>
+          <Button asChild variant="link" size="lg" className="hover-btn p-0">
+            <Link to="/register">Register</Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default UserBar;
