3d5c55131388cd2ff4d49f4550d7b59a2113a21a
shadow-developper <axel.barth@yahoo.com>
2025-07-23 11:31:30 +0200

    Footer - Épisode n°99

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,6 @@
+import { col1, col2, col3, col4, colTitles, icons, text } from "../utils/footerData";
+import logo from "../assets/images/Logo.webp";
+
 const Footer = () => {
   return (
 	<div>Footer</div>
---
+++ src/utils/footerData.tsx
@@ -0,0 +1,23 @@
+import stripe from "../assets/images/icons/stripe-svgrepo-com.svg";
+import visa from "../assets/images/icons/visa-logo-svgrepo-com.svg";
+import master from "../assets/images/icons/mastercard-old-svgrepo-com.svg";
+import iata from "../assets/images/icons/iata-svgrepo-com.svg";
+import amex from "../assets/images/icons/amex-svgrepo-com.svg";
+import bitcoin from "../assets/images/icons/bitcoin-svgrepo-com.svg";
+
+const icons = { stripe, visa, master, iata, amex, bitcoin };
+
+const text: string =
+	"France's leading network of independent travel agencies, HexaTrip offers you the expertise of over 200 passionate advisors to design the trip of your dreams for more than 20 years. Whether you're looking for a getaway, a tour, a cruise, a flight, a hotel, or an unforgettable experience, we provide a wide range of tailor-made travel options, available online or through our 20 agencies across the country.";
+
+const colTitles: string[] = ["About Us", "Commitment", "Contacts", "Legal information"];
+
+const col1: string[] = ["Who are we ?", "HexaTrip Academy", "Join us", "Business trip", "All our destinations"];
+
+const col2: string[] = ["HexaTrip Foundation", "Why choose HexaTrip?"];
+
+const col3: string[] = ["Contact us", "Press area"];
+
+const col4: string[] = ["Legal Notices", "General conditions of sale", "Cookie Consent Management"];
+
+export { icons, text, colTitles, col1, col2, col3, col4 };
