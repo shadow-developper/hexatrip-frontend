06e07c79d66012408e43be2e61573be22d06fc62
shadow-developper <axel.barth@yahoo.com>
2025-07-24 12:56:49 +0200

    Navbar mobile - Épisode n°105

--- src/components/LinksMobile.tsx
+++ src/components/LinksMobile.tsx
@@ -1,7 +1,39 @@
+import { AlignLeft } from "lucide-react";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { links } from "@/utils/navbarData";
+import { Link } from "@/types/types";
+import { NavLink, useLocation } from "react-router-dom";
+
 const LinksMobile = () => {
+  const location = useLocation();
+
   return (
-	<div>LinksMobile</div>
-  )
+  <DropdownMenu>
+    <DropdownMenuTrigger asChild className="lg:hidden">
+      <Button variant="outline" size="icon">
+        <AlignLeft />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className="lg:hidden" align="start" sideOffset={10}>
+      			{links.map((link) => {
+				const { ref, label } = link as Link;
+				const isActive = location.pathname + location.search === ref;
+				return (
+          <DropdownMenuItem key={label}>
+					<NavLink
+						to={ref}
+						key={label}
+						className={`capitalize tracking-wide text-black hover:text-amber-600 ${isActive ? "underline" : ""}`}
+					>
+						{label}
+					</NavLink>
+          </DropdownMenuItem>
+				);
+			})}
+    </DropdownMenuContent>
+  </DropdownMenu>
+)
 }
 
 export default LinksMobile;
