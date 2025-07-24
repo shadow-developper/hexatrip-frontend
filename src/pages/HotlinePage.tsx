7de791a924e8f7ad72995f524075300ec9e57017
shadow-developper <axel.barth@yahoo.com>
2025-07-24 18:28:34 +0200

    Hotline style - Épisode n°107

--- src/pages/HotlinePage.tsx
+++ src/pages/HotlinePage.tsx
@@ -3,14 +3,14 @@ import { hotlineData } from "@/utils/hotlineData";
 
 const HotlinePage = () => {
 	return (
-		<section className="">
-			<div className="">
-				<div className="">
-					<Title text="hotline" classname="" />
-					<div className="">
-						<img src={hotlineData.src} alt="hotline-logo" className="" />
+		<section className="my-8 min-h-[50vh]">
+			<div className="align-center">
+				<div className="flex flex-col gap-8 md:flex-row md:items-start">
+					<Title text="Hotline" classname="m-0 p^-0 text-4xl capitalize"/>
+					<div className="h-[300px] w-[300px] shrink-0">
+						<img src={hotlineData.src} alt="hotline-logo" className="h-full w-full object-cover rounded-full" />
 					</div>
-					<div className="">
+					<div className="max-w-[50%] mt-6">
 						<p className="">{hotlineData.text}</p>
 					</div>
 				</div>
